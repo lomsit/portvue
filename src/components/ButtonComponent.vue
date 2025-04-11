bbb85685fe3b2c19e8294e806f4141e4e272193d
Alex <aleksey.potato@gmail.com>
2025-04-12 01:11:37 +0300

    link  changed

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -4,7 +4,7 @@
       <my-button href="https://t.me/lomsit" class="btn__tg">My Telegram</my-button>
     </div>  
     <div class="btn__column animate__animated animate__fadeInDown">
-      <my-button href="https://krasnodar.hh.ru/applicant/resumes/view?resume=89f96043ff0e21e1230039ed1f6c4e394b7670">My Resume on HH.ru</my-button>
+      <my-button href="https://krasnodar.hh.ru/resume/e47ac4aeff0ea5b1170039ed1f4c576354494d">My Resume on HH.ru</my-button>
     </div>  
     <div class="btn__column animate__animated animate__fadeInRight animate__delay-1s">
       <my-button href="https://github.com/lomsit" class="btn__git">My GitHub</my-button>
